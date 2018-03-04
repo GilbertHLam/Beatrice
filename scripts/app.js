d81a978814a1d13c7805c938d1e5acbb78fe0dfd
GilbertHLam <gilberthlam@cmail.carleton.ca>
2018-03-04 14:06:53 -0500

    Added animations and information

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
 particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+function goResume(){
+  $('html,body').animate({
+      scrollTop: $("#resume").offset().top
+  },
+      2000);
+}
